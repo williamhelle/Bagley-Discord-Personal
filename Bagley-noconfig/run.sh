a2c4261f4c4e8e04ff00f1ffa25350a773243d39
Williamhelle <williamhelle@icloud.com>
2022-10-31 18:38:40 +0100

    Update run.sh

--- Bagley-noconfig/run.sh
+++ Bagley-noconfig/run.sh
@@ -1,21 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
 
-
-# Create main config
-BOTPY=$(bashio::config 'botpy')
-DIRBOT=$(bashio::config 'dirbot')
-INSTALLPY=$(bashio::config 'installpy')
-
-
-
-
 bashio::log.info "-------------------------------------------------------------"
 bashio::log.info "Récapitulatif des configurations pour le bot"
 bashio::log.info "-------------------------------------------------------------"
-echo "option botpy \"${BOTPY}\";"
-echo "option dirbot \"${DIRBOT}\";"
-echo "option installpy \"${INSTALLPY}\";"
+echo "botpy = bot.py"
+echo "dirbot = share/Discord"
+echo "installpy = disnake"
 bashio::log.info "-------------------------------------------------------------"
 
 
@@ -28,23 +19,23 @@ bashio::log.info "-------------------------------------------------------------"
 bashio::log.info "Installation des composants personnalisé pour le bot (python)"
 bashio::log.info "-------------------------------------------------------------"
 
-pip install ${INSTALLPY}
+pip install disnake
 bashio::log.info "Success"
 
 bashio::log.info "-------------------------------------------------------------"
 bashio::log.info "Lancement du bot Discord"
 bashio::log.info "-------------------------------------------------------------"
 
-echo "Entre dans le dossier (~/${DIRBOT})"
-cd ${DIRBOT}
+echo "Entre dans le dossier (~/share/Discord)"
+cd share/Discord
 bashio::log.info "Success"
 
 bashio::log.info "-------------------------------------------------------------"
 bashio::log.info "Lancement du script du bot Discord en python"
-echo "python3 (~/${DIRBOT}/${BOTPY})"
+echo "python3 (~/share/Discord/bot.py)"
 bashio::log.info "-------------------------------------------------------------"
 
-python3 ${BOTPY}
+python3 bot.py
 bashio::log.info "Le bot s'est arrêté ou à crash"
 bashio::log.info "Arrêt du bot Discord"
 
