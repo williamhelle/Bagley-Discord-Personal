ee10692264d344a37e1777cd7697ce97548ce590
Williamhelle <williamhelle@icloud.com>
2022-10-31 17:42:40 +0100

    Update run.sh

--- Bagley/run.sh
+++ Bagley/run.sh
@@ -1,15 +1,52 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Lancement du bot Discord"
-echo "Entre dans le dossier (~/share)"
-cd share
-echo "Success"
 
-echo "Entre dans le dossier (~/share/Discord)"
-cd Discord
-echo "Success"
 
-echo "Lancement du script du bot Discord en python"
+# Create main config
+INSTALLPY=$(bashio::config 'installpy')
+DIRBOT=$(bashio::config 'dirbot')
+
+
+
+
+bashio::log.info "-------------------------------------------------------------"
+bashio::log.info "Récapitulatif des configurations pour le bot"
+bashio::log.info "-------------------------------------------------------------"
+echo "option installpy \"${INSTALLPY}\";"
+echo "option dirbot \"${DIRBOT}\";"
+bashio::log.info "-------------------------------------------------------------"
+
+
+
+
+
+
+
+bashio::log.info "-------------------------------------------------------------"
+bashio::log.info "Installation des composants personnalisé pour le bot (python)"
+bashio::log.info "-------------------------------------------------------------"
+
+pip install ${INSTALLPY}
+bashio::log.info "Success"
+
+bashio::log.info "-------------------------------------------------------------"
+bashio::log.info "Lancement du bot Discord"
+bashio::log.info "-------------------------------------------------------------"
+
+echo "Entre dans le dossier (~/${DIRBOT})"
+cd ${DIRBOT}
+bashio::log.info "Success"
+
+bashio::log.info "-------------------------------------------------------------"
+bashio::log.info "Lancement du script du bot Discord en python"
+bashio::log.info "-------------------------------------------------------------"
+
 python3 bot.py
-echo "Le bot s'est arrêté ou à crash"
-echo "Arrêt du bot Discord"
+bashio::log.info "Le bot s'est arrêté ou à crash"
+bashio::log.info "Arrêt du bot Discord"
+
+
+
+bashio::log.info "-------------------------------------------------------------"
+bashio::log.info "Arrêt de l'addon bot Discord"
+bashio::log.info "-------------------------------------------------------------"
